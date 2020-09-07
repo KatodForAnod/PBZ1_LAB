e4ab02bea818b9e27e47328ea96ef3e39526461b
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-09-07 17:41:02 +0300

    create method for print table realise task 1.1

--- src/base_pakage/Main.java
+++ src/base_pakage/Main.java
@@ -95,6 +95,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        TestDatabase();
+        WorkWithDatabase a = new WorkWithDatabase();
     }
 }
--- src/base_pakage/WorkWithDatabase.java
+++ src/base_pakage/WorkWithDatabase.java
@@ -2,22 +2,62 @@ package base_pakage;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.stream.Stream;
 
 public class WorkWithDatabase {
     private Connection c;
     private Statement stmt;
 
-    public void WorkWithDatabase() {
+    public WorkWithDatabase() {
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
                     .getConnection("jdbc:postgresql://localhost:4444/testbase", "postgres", "12345678");
-            //c.setAutoCommit(false);
+
+            //getInformationAboutAllTeachers();
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
+    }
+
+    private void printTable(int counter, String... strings) {
+        System.out.print("\n" + counter);
+        for (String i : strings) {
+            System.out.print("| " + i + " | ");
+        }
+    }
+
+    private void getInformationAboutAllTeachers() {
+        try {
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM teachers;");
+
+            int counter = 1;
+            while (rs.next()) {
+
+                String personalNumber = rs.getString("personal_number");
+                String surname = rs.getString("surname");
+                String position = rs.getString("position");
+                String department = rs.getString("department");
+                String speciality = rs.getString("speciality");
+                String homeNumber = String.valueOf(rs.getInt("home_number"));
+
+                printTable(counter, personalNumber, surname, position, department,
+                        speciality, homeNumber);
+                counter++;
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
     }
 }
