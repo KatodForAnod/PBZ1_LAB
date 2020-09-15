c076a5f0a6823653d890ccd3e3764e99a39458dd
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-09-15 17:41:38 +0300

    add new species in class

--- src/base_pakage/CreateDatabase.java
+++ src/base_pakage/CreateDatabase.java
@@ -26,11 +26,31 @@ public class CreateDatabase {
         try {
             String sql;
             stmt = c.createStatement();
+            sql = "CREATE TABLE SUPPLIERS " +
+                    "(P               TEXT PRIMARY KEY     NOT NULL," +
+                    " NAME_P          TEXT     NOT NULL, " +
+                    " STATUS          INT      NOT NULL, " +
+                    " CITY            TEXT     NOT NULL)";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE DETAILS " +
+                    "(D               TEXT PRIMARY KEY     NOT NULL," +
+                    " NAME_D          TEXT     NOT NULL, " +
+                    " COLOR           TEXT     NOT NULL, " +
+                    " SIZE            INT      NOT NULL," +
+                    " CITY            TEXT     NOT NULL)";
+            stmt.executeUpdate(sql);
+
+            sql = "CREATE TABLE PROJECTS " +
+                    "(PR               TEXT PRIMARY KEY     NOT NULL," +
+                    " NAME_PR          TEXT     NOT NULL, " +
+                    " CITY             TEXT     NOT NULL)";
+            stmt.executeUpdate(sql);
 
             sql = "CREATE TABLE SUPPLIER_DETAIL_PROJECT_AMOUNT " +
                     "(P         TEXT      NOT NULL," +
-                    " D         TEXT     NOT NULL, " +
-                    " PR        TEXT    NOT NULL," +
+                    " D         TEXT      NOT NULL, " +
+                    " PR        TEXT      NOT NULL," +
                     " S         INT NOT NULL)";
             stmt.executeUpdate(sql);
 
@@ -43,6 +63,15 @@ public class CreateDatabase {
     }
 
     private void insertRows() {
-
+        try {
+            String sql;
+            stmt = c.createStatement();
+            sql = "INSERT INTO  (CODE_NUMBER_GROUP,CODE_NUMBER_SUBJECT,PERSONAL_NUMBER,AUDIENCE_NUMBER) VALUES ('10G', '12P', '222L', 210);";
+            stmt.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
     }
 }
