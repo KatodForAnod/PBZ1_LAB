ea56eb836abea2c630162cd481c58cc949a3310e
Nikita Makarchuk <vfrfrfcbys@yandex.by>
2020-09-15 18:35:03 +0300

    Add realization method insertRows

--- src/base_pakage/CreateDatabase.java
+++ src/base_pakage/CreateDatabase.java
@@ -13,7 +13,7 @@ public class CreateDatabase {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
                     .getConnection("jdbc:postgresql://localhost:4444/2ndDatabase", "postgres", "12345678");
-
+            insertRows();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -66,8 +66,62 @@ public class CreateDatabase {
         try {
             String sql;
             stmt = c.createStatement();
-            sql = "INSERT INTO  (CODE_NUMBER_GROUP,CODE_NUMBER_SUBJECT,PERSONAL_NUMBER,AUDIENCE_NUMBER) VALUES ('10G', '12P', '222L', 210);";
+
+            sql = "INSERT INTO SUPPLIERS (P,NAME_P,STATUS,CITY) VALUES " +
+                    "('P1','Петров',20,'Москва')," +
+                    "('P2','Синицин',10,'Таллинн')," +
+                    "('P3','Федоров',30,'Таллинн')," +
+                    "('P4','Чаянов',20,'Минск')," +
+                    "('P5','Крюков',30,'Киев');";
             stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO DETAILS (D,NAME_D,COLOR,SIZE,CITY) VALUES " +
+                    "('D1','Болт','Красный',12,'Москва')," +
+                    "('D2','Гайка','Зеленый',17,'Минск')," +
+                    "('D3','Диск','Черный',17,'Вильнюс')," +
+                    "('D4','Диск','Черный',14,'Москва')," +
+                    "('D5','Корпус','Красный',12,'Минск')," +
+                    "('D6','Крышки','Красный',19,'Москва');";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO PROJECTS (PR,NAME_PR,CITY) VALUES " +
+                    "('PR1','ИПР1','Минск')," +
+                    "('PR2','ИПР2','Таллинн')," +
+                    "('PR3','ИПР3','Псков')," +
+                    "('PR4','ИПР4','Псков')," +
+                    "('PR5','ИПР5','Москва')," +
+                    "('PR6','ИПР6','Саратов')," +
+                    "('PR7','ИПР7','Москва');";
+            stmt.executeUpdate(sql);
+
+            sql = "INSERT INTO SUPPLIER_DETAIL_PROJECT_AMOUNT (P,D,PR,S) VALUES " +
+                    "('P1','D1','PR1',200)," +
+                    "('P1','D1','PR2',700)," +
+                    "('P2','D3','PR1',400)," +
+                    "('P2','D2','PR2',200)," +
+                    "('P2','D3','PR3',200)," +
+                    "('P2','D3','PR4',500)," +
+                    "('P2','D3','PR5',600)," +
+                    "('P2','D3','PR6',400)," +
+                    "('P2','D3','PR7',800)," +
+                    "('P2','D5','PR2',100)," +
+                    "('P3','D3','PR1',200)," +
+                    "('P3','D4','PR2',500)," +
+                    "('P4','D6','PR3',300)," +
+                    "('P4','D6','PR7',300)," +
+                    "('P5','D2','PR2',200)," +
+                    "('P5','D2','PR4',100)," +
+                    "('P5','D5','PR5',500)," +
+                    "('P5','D5','PR7',100)," +
+                    "('P5','D6','PR2',200)," +
+                    "('P5','D1','PR2',100)," +
+                    "('P5','D3','PR4',200)," +
+                    "('P5','D4','PR4',800)," +
+                    "('P5','D5','PR4',400)," +
+                    "('P5','D6','PR4',500);";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
